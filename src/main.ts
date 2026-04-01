84fa5160a5991ded41642f55a1a5adc7a121696b
EmanoelLimaGomes <emanoelgomes97003493@gmail.com>
2026-03-31 21:31:04 -0300

    Validation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { Reflector } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
@@ -6,6 +7,14 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const reflector = app.get(Reflector);
   app.useGlobalInterceptors(new HateoasInterceptor(reflector));
